db74a160b6dd6ee77d362a7a787f8e7ab089730c
Giktg <disktg2021@gmail.com>
2022-11-10 16:08:49 +0300

    Update life_gui.py

--- homework03/life_gui.py
+++ homework03/life_gui.py
@@ -16,7 +16,6 @@ class GameOfLife:
     def __init__(
         self, width: int = 640, height: int = 480, cell_size: int = 10, speed: int = 10
     ) -> None:
-        self.past = Past
         self.width = width
         self.height = height
         self.cell_size = cell_size
